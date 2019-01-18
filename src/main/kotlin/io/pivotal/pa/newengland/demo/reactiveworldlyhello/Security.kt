c2d6b1c4be5654104d7c47693dff899c8f7eff85
Chuck D'Antonio <c_dantonio@acm.org>
2019-01-17 21:12:45 -0500

    Tune up security

--- src/main/kotlin/io/pivotal/pa/newengland/demo/reactiveworldlyhello/Security.kt
+++ src/main/kotlin/io/pivotal/pa/newengland/demo/reactiveworldlyhello/Security.kt
@@ -1,2 +1,22 @@
 package io.pivotal.pa.newengland.demo.reactiveworldlyhello
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+  @Bean
+  fun permitAllSecurity(http: ServerHttpSecurity): SecurityWebFilterChain =
+    http
+      .csrf().disable()
+      .authorizeExchange().pathMatchers("/greeting/**", "/greetings").permitAll()
+      .and()
+      .build()
+
+}
